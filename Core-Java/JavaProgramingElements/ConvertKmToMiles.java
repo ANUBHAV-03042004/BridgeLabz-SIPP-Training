a30f37449d40ee497d3d77366424d57ccbf2b3a9
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-06-18 10:33:00 +0530

    [Anubhav Kumar Srivastava] Add. Convert distance in Kilometers into Miles

--- Core-Java/JavaProgramingElements/ConvertKmToMiles.java
+++ Core-Java/JavaProgramingElements/ConvertKmToMiles.java
@@ -1,7 +1,9 @@
 public class ConvertKmToMiles {
     public static void main(String[] args) {
+//    	take input for kilometers
         double kilometers = 10.8;
+//        multiply it with 1.6 to convert into miles
         double miles = kilometers * 1.6;        
         System.out.println("The distance " + kilometers + " km in miles is " + miles);
     }
-}
+} 
