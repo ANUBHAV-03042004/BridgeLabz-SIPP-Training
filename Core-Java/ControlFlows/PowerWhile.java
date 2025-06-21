f700a8ea6f707122107da2a27994bdacc4efef33
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-06-21 22:10:25 +0530

    [Anubhav Kumar Srivastava] Add. Calculated the result by multiplying the number with itself till the counter is equal to the power entered by the user.

--- Core-Java/ControlFlows/PowerWhile.java
+++ Core-Java/ControlFlows/PowerWhile.java
@@ -3,19 +3,25 @@ import java.util.Scanner;
 public class PowerWhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // Take base number input
         System.out.print("Enter base number: ");
         int number = sc.nextInt();
+
+        // Take exponent input
         System.out.print("Enter power: ");
         int power = sc.nextInt();
 
-        int result = 1;
-        int counter = 0;
+        int result = 1;        // Initial result
+        int counter = 0;       // Counter variable for loop
 
+        // Loop till counter equals power
         while (counter < power) {
-            result *= number;
-            counter++;
+            result *= number;  // Multiply result by base each time
+            counter++;         // Increment counter
         }
 
+        // Print final result
         System.out.println("Result: " + result);
     }
 }
