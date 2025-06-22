f7794988c92f06a831d1f6d355577c425c17c845
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-06-22 13:27:15 +0530

    [Anubhav Kumar Srivastava] Add. Find whether the number is multipe of 3 or fizz number or a multiple of 5 a buzz number or a both a FizzBuzz number store the result in an array and display it.

--- Core-Java/Arrays/FizzBuzzInArray.java
+++ Core-Java/Arrays/FizzBuzzInArray.java
@@ -14,11 +14,14 @@ public class FizzBuzzInArray {
         String[] result = new String[number + 1];
 
         for (int i = 1; i <= number; i++) {
+//        	if multiple of both store "fizzbuzz"
             if (i % 3 == 0 && i % 5 == 0) {
                 result[i] = "FizzBuzz";
             } else if (i % 3 == 0) {
+//            	store "fizz" if multiple of 3
                 result[i] = "Fizz";
             } else if (i % 5 == 0) {
+//            	store "buzz" if multiple of 5
                 result[i] = "Buzz";
             } else {
                 result[i] = String.valueOf(i);
