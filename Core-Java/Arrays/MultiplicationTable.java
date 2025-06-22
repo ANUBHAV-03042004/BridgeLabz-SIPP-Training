e63d7d6728253a1c7823037203189a5c94b2ec21
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-06-22 13:28:21 +0530

    [Anubhav Kumar Srivastava] Add. Multiply the user input number with 1 to 10 and store result in the array and display it.

--- Core-Java/Arrays/MultiplicationTable.java
+++ Core-Java/Arrays/MultiplicationTable.java
@@ -11,7 +11,7 @@ public class MultiplicationTable {
         for (int i = 1; i <= 10; i++) {
             table[i - 1] = number * i;
         }
-
+//  multiplication from 1-10 with user's input
         for (int i = 0; i < 10; i++) {
             System.out.println(number + " * " + (i + 1) + " = " + table[i]);
         }
