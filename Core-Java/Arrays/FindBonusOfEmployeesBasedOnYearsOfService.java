e06b1f009a845f5a58e617ffe9ac442a76bfb1e1
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-06-22 15:26:02 +0530

    [Anubhav Kumar Srivastava] Add. Find the Bonus of employee based on years of service.

--- Core-Java/Arrays/FindBonusOfEmployeesBasedOnYearsOfService.java
+++ Core-Java/Arrays/FindBonusOfEmployeesBasedOnYearsOfService.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FindBonusOfEmployeesBasedOnYearsOfService {
+public class FindBonusOfEmployeesBasedOnYearsOfService{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = 10;
