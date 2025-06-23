932c79d34c125d9acfc801905c178b3d5605deda
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-06-23 23:46:38 +0530

    [Anubhav Kumar Srivastava] Add. Method to convert fahrenheit into celsius and gallons to liters and vice versa.

--- Core-Java/Methods/RootsOfQuadraticEquation.java
+++ Core-Java/Methods/RootsOfQuadraticEquation.java
@@ -17,8 +17,11 @@ public class RootsOfQuadraticEquation {
     public static void main(String[] args) {
     	Scanner sc=new Scanner(System.in);
 //    	user's input  a , b and c
+    	System.out.println("Enter value of a:");
     	int a=sc.nextInt();
+    	System.out.println("Enter value of b:");
     	int b=sc.nextInt();
+    	System.out.println("Enter value of c:");
     	int c=sc.nextInt();
         double[] roots = findRoots(a,b,c); 
         System.out.println("Roots: " + Arrays.toString(roots));
