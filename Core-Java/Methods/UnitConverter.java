fe6fa70d35fd0d94e084616da08f2293732e883b
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-06-23 23:27:27 +0530

    [Anubhav Kumar Srivastava] Add. Method to convert km to miles and meters to feet and vice versa.

--- Core-Java/Methods/UnitConverter.java
+++ Core-Java/Methods/UnitConverter.java
@@ -1,16 +1,19 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class UnitConverter {
+//	method to convert Km To Miles
     public static double convertKmToMiles(double km) {
         return km * 0.621371;
     }
+//	method to convert Miles To Km
     public static double convertMilesToKm(double miles) {
         return miles * 1.60934;
     }
+//	method to convert meters To feet
     public static double convertMetersToFeet(double m) {
         return m * 3.28084;
     }
+//	method to convert feet to meters 
     public static double convertFeetToMeters(double ft) {
         return ft * 0.3048;
     }
