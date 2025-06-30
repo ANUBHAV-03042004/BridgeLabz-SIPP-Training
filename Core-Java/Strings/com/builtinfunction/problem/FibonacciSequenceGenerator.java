2fc6bcda59a861b37d81062e97996f72203efccb
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-06-30 09:54:17 +0530

    [Anubhav Kumar Srivastava] Add. Input no. of sequence user wants of a fibonacci sequence and generate and display that.

---
+++ Core-Java/Strings/com/builtinfunction/problem/FibonacciSequenceGenerator.java
@@ -0,0 +1,22 @@
+package com.builtinfunction.problem;
+import java.util.Scanner;
+public class FibonacciSequenceGenerator {
+	    public static void generateFibonacci(int terms) {
+	        int a = 0, b = 1;
+	        for (int i = 0; i < terms; i++) {
+	            System.out.print(a + " ");
+	            int temp = b;
+	            b = a + b;
+	            a = temp;
+	        }
+	        System.out.println();
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter number of terms: ");
+	        int n = sc.nextInt();
+	        generateFibonacci(n);
+	        sc.close();
+	    }
+}
