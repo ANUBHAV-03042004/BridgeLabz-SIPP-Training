ad2a9f18689c921ba1b8a301dd058710eb017f1e
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-06-30 09:49:19 +0530

    [Anubhav Kumar Srivastava] Add. Input three number and find the maximum among them using built in max function.

---
+++ Core-Java/Strings/com/builtinfunction/problem/FindOutMaximumAmongThreeNumbers.java
@@ -0,0 +1,17 @@
+package com.builtinfunction.problem;
+import java.util.Scanner;
+public class FindOutMaximumAmongThreeNumbers {
+	    public static int max(int a, int b, int c) {
+	        return Math.max(a, Math.max(b, c));
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter 3 numbers: ");
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        int c = sc.nextInt();
+	        System.out.println("Maximum is: " + max(a, b, c));
+	        sc.close();
+	    }
+	}
