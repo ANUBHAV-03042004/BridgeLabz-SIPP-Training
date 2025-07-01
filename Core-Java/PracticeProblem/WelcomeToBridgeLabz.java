7b010ab30689001e820a11d8b4a69e70a09d643f
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-07-01 10:31:49 +0530

    [Anubhav Kumar Srivastava] Add. Created a method to print a message.

---
+++ Core-Java/PracticeProblem/WelcomeToBridgeLabz.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class WelcomeToBridgeLabz {
+	 // Welcome Message
+	 public static void welcomeMessage() {
+	     System.out.println("Welcome to Bridgelabz!");
+	 }
+	 public static void main(String[] args) {
+	     Scanner sc = new Scanner(System.in);
+	      welcomeMessage();
+	 }
+}
+
+
+
+
+
+
+
