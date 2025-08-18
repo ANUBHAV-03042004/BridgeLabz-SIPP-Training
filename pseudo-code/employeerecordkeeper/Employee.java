312d2ffe3b0c25b4a314675d87f0d94edc90b3d1
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-08-18 17:11:39 +0530

    [Anubhav Kumar Srivastava] Add. Used arraylist and streams to implement employee record keeper.

---
+++ pseudo-code/employeerecordkeeper/Employee.java
@@ -0,0 +1,14 @@
+package employeerecordkeeper;
+
+import java.util.*;
+
+
+class Employee {
+ String name;
+ String department;
+
+ public Employee(String name, String department) {
+     this.name = name;
+     this.department = department;
+ }
+}
