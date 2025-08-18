c96e7a9d38fc0b53358b265ac39ef7fcbcc88d62
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-08-18 21:19:26 +0530

    [Anubhav Kumar Srivastava] Add. Used lambda function to implement smart home lighting system.

---
+++ lambdaexpression/smarthomelightingsystem/SmartHomeLightingSystem.java
@@ -0,0 +1,20 @@
+package smarthomelightingsystem;
+
+interface LightAction {
+ void activate();
+}
+
+public class SmartHomeLightingSystem {
+ public static void main(String[] args) {
+     // Lambda for different triggers
+     LightAction motionTrigger = () -> System.out.println("Lights ON: Motion detected!");
+     LightAction timeTrigger = () -> System.out.println("Lights DIMMED: Evening mode activated.");
+     LightAction voiceTrigger = () -> System.out.println("Lights OFF: Voice command received.");
+
+     // Executing behaviors dynamically
+     motionTrigger.activate();
+     timeTrigger.activate();
+     voiceTrigger.activate();
+ }
+}
+
