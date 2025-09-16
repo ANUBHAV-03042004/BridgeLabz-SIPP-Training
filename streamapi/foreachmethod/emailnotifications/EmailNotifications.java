06d6ecd838be276ea51786248d274183f2665f5c
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-09-16 23:00:43 +0530

    [Anubhav Kumar Srivastava] Add. Used for each method to print all values from arraylist and create send email notification method to send to all users using loop.

---
+++ streamapi/foreachmethod/emailnotifications/EmailNotifications.java
@@ -0,0 +1,14 @@
+package foreachmethod.emailnotifications;
+	import java.util.*;
+
+	public class EmailNotifications {
+	    public static void main(String[] args) {
+	        List<String> emails = Arrays.asList("user1@example.com", "user2@example.com");
+
+	        emails.forEach(email -> sendEmailNotification(email));
+	    }
+
+	    static void sendEmailNotification(String email) {
+	        System.out.println("Sending email to: " + email);
+	    }
+	}
