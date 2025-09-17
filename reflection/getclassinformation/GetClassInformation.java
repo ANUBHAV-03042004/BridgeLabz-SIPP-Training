fd57f33025cc2ecb8b4dea996982b29d61368f2e
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-09-17 23:33:21 +0530

    [Anubhav Kumar Srivastava] Add. Fetched all information details using reflection.

---
+++ reflection/getclassinformation/GetClassInformation.java
@@ -0,0 +1,25 @@
+package getclassinformation;
+
+	import java.lang.reflect.*;
+	public class GetClassInformation {
+	    public static void main(String[] args) throws Exception {
+	        Class<?> clazz = Class.forName("java.util.ArrayList");
+
+	        System.out.println("Class Name: " + clazz.getName());
+
+	        System.out.println("\n--- Constructors ---");
+	        for (Constructor<?> cons : clazz.getDeclaredConstructors()) {
+	            System.out.println(cons);
+	        }
+
+	        System.out.println("\n--- Fields ---");
+	        for (Field f : clazz.getDeclaredFields()) {
+	            System.out.println(f);
+	        }
+
+	        System.out.println("\n--- Methods ---");
+	        for (Method m : clazz.getDeclaredMethods()) {
+	            System.out.println(m);
+	        }
+	    }
+	}
