40506a491c92a371f71236af292cc5888d9f9b3f
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-09-18 14:13:01 +0530

    [Anubhav Kumar Srivastava] Add. Used override correctly using annotations.

---
+++ annotation/basiclevel/UseOverrideCorrectly.java
@@ -0,0 +1,21 @@
+package basiclevel;
+
+	class Animal {
+	    public void makeSound() {
+	        System.out.println("Animal makes a sound");
+	    }
+	}
+
+	class Dog extends Animal {
+	    @Override
+	    public void makeSound() {
+	        System.out.println("Dog barks 🐶");
+	    }
+	}
+
+	public class UseOverrideCorrectly {
+	    public static void main(String[] args) {
+	        Animal a = new Dog();
+	        a.makeSound();
+	    }
+	}
